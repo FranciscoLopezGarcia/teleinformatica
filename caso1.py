906892961ced415b14f092bf8ff29f68fa39bab8
FranciscoLopezGarcia <franloga02@gmail.com>
2025-05-15 21:59:16 -0300

    functional

--- caso1.py
+++ caso1.py
@@ -1,93 +1,87 @@
+from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.node import Node, OVSKernelSwitch, Host
+from mininet.node import Node, OVSKernelSwitch
+from mininet.link import TCLink
 from mininet.cli import CLI
-from mininet.log import setLogLevel, info
+from mininet.log import setLogLevel
 
-def nationalWanNetwork():
-    "Crea una red WAN nacional con Casa Matriz y dos sucursales."
+class LinuxRouter(Node):
+    def config(self, **params):
+        super().config(**params)
+        self.cmd('sysctl -w net.ipv4.ip_forward=1')
 
-    net = Mininet(topo=None, build=False, controller=None) # Sin controlador por defecto, IPs manuales
+    def terminate(self):
+        self.cmd('sysctl -w net.ipv4.ip_forward=0')
+        super().terminate()
 
-    info('*** Agregando Routers (Nodos Linux con reenvío IP)\n')
-    # Router Casa Matriz
-    r0 = net.addHost('r0', cls=Node, ip=None) # IP principal no es crucial, las interfaces sí
-    r0.cmd('sysctl -w net.ipv4.ip_forward=1')
+def dynamicNationalWanNetwork():
+    num_sucursales = int(input("Ingrese la cantidad de sucursales a conectar (mínimo 1): "))
+    print(f"*** Creando red para {num_sucursales} sucursal(es) ***")
 
-    # Router Sucursal 1
-    r1 = net.addHost('r1', cls=Node, ip=None)
-    r1.cmd('sysctl -w net.ipv4.ip_forward=1')
+    net = Mininet(link=TCLink, switch=OVSKernelSwitch, controller=None)
 
-    # Router Sucursal 2
-    r2 = net.addHost('r2', cls=Node, ip=None)
-    r2.cmd('sysctl -w net.ipv4.ip_forward=1')
+    routers = []
+    switches_wan = []
+    switches_lan = []
+    hosts = []
 
-    info('*** Agregando Hosts de Sucursales\n')
-    # Host en Sucursal 1
-    # La IP se asigna con máscara y la ruta por defecto a través del router de su LAN
-    h1 = net.addHost('h1', cls=Host, ip='10.0.1.254/24', defaultRoute='via 10.0.1.1')
+    # Router principal
+    r0 = net.addHost('r0', cls=LinuxRouter, ip=None)
+    print("*** Agregando Router Casa Matriz (r0)")
+    routers.append(r0)
 
-    # Host en Sucursal 2
-    h2 = net.addHost('h2', cls=Host, ip='10.0.2.254/24', defaultRoute='via 10.0.2.1')
+    for i in range(1, num_sucursales + 1):
+        ri = net.addHost(f'r{i}', cls=LinuxRouter, ip=None)
+        hi = net.addHost(f'h{i}', ip=f'10.0.{i}.254/24', defaultRoute=f'via 10.0.{i}.1')
+        swi_wan = net.addSwitch(f's{i}_wan')
+        swi_lan = net.addSwitch(f's{i}_lan')
 
-    info('*** Agregando Switches\n')
-    # Switches para enlaces WAN (simulando el medio físico)
-    s1_wan = net.addSwitch('s1_wan', cls=OVSKernelSwitch, failMode='standalone')
-    s2_wan = net.addSwitch('s2_wan', cls=OVSKernelSwitch, failMode='standalone')
+        print(f"    Agregado Router Sucursal: r{i}")
+        print(f"    Agregado Host Sucursal: h{i} (IP: 10.0.{i}.254/24)")
+        print(f"    Agregado Switch WAN: s{i}_wan")
+        print(f"    Agregado Switch LAN: s{i}_lan")
 
-    # Switches para LANs de sucursales
-    s1_lan = net.addSwitch('s1_lan', cls=OVSKernelSwitch, failMode='standalone')
-    s2_lan = net.addSwitch('s2_lan', cls=OVSKernelSwitch, failMode='standalone')
+        routers.append(ri)
+        hosts.append(hi)
+        switches_wan.append(swi_wan)
+        switches_lan.append(swi_lan)
 
-    info('*** Creando Enlaces WAN y asignando IPs\n')
-    # Enlace WAN Sucursal 1: r0 <-> s1_wan <-> r1
-    net.addLink(r0, s1_wan, intfName1='r0-eth0', params1={'ip': '192.168.100.6/29'})
-    net.addLink(r1, s1_wan, intfName1='r1-wan0', params1={'ip': '192.168.100.1/29'})
+    print("*** Creando Enlaces y asignando IPs")
 
-    # Enlace WAN Sucursal 2: r0 <-> s2_wan <-> r2
-    net.addLink(r0, s2_wan, intfName1='r0-eth1', params1={'ip': '192.168.100.14/29'})
-    net.addLink(r2, s2_wan, intfName1='r2-wan0', params1={'ip': '192.168.100.9/29'})
+    for i in range(1, num_sucursales + 1):
+        wan_net_base = 192 + i  # puede ser 192.168.100.X + offset
+        wan_prefix = f"192.168.100.{8*i - 1}/29"
+        wan_r0_ip = f"192.168.100.{8*i - 2}/29"
+        wan_rn_ip = f"192.168.100.{8*i - 3}/29"
 
-    info('*** Creando Enlaces LAN y asignando IPs\n')
-    # LAN Sucursal 1: r1 <-> s1_lan <-> h1
-    net.addLink(r1, s1_lan, intfName1='r1-lan0', params1={'ip': '10.0.1.1/24'})
-    net.addLink(h1, s1_lan, intfName1='h1-eth0') # IP ya definida en addHost
+        # WAN
+        net.addLink(routers[0], switches_wan[i - 1])
+        net.addLink(routers[i], switches_wan[i - 1])
+        routers[0].cmd(f'ip addr add {wan_r0_ip} dev r0-eth{i - 1}')
+        routers[i].cmd(f'ip addr add {wan_rn_ip} dev r{i}-eth0')
 
-    # LAN Sucursal 2: r2 <-> s2_lan <-> h2
-    net.addLink(r2, s2_lan, intfName1='r2-lan0', params1={'ip': '10.0.2.1/24'})
-    net.addLink(h2, s2_lan, intfName1='h2-eth0') # IP ya definida en addHost
+        print(f"    Enlace WAN Suc.{i}: r0(r0-eth{i - 1} [{wan_r0_ip}]) <-> s{i}_wan <-> r{i}(r{i}-wan0 [{wan_rn_ip}])")
 
-    info('*** Iniciando Red\n')
-    net.build()
+        # LAN
+        net.addLink(routers[i], switches_lan[i - 1])
+        net.addLink(hosts[i - 1], switches_lan[i - 1])
 
-    info('*** Iniciando Switches\n')
-    net.get('s1_wan').start([])
-    net.get('s2_wan').start([])
-    net.get('s1_lan').start([])
-    net.get('s2_lan').start([])
+        routers[i].cmd(f'ip addr add 10.0.{i}.1/24 dev r{i}-eth1')
+        print(f"    Enlace LAN Suc.{i}: r{i}(r{i}-lan0 [10.0.{i}.1/24]) <-> s{i}_lan <-> h{i}(h{i}-eth0 [10.0.{i}.254/24])")
 
-    info('*** Configurando Rutas Estáticas en Routers\n')
-    # Rutas en Router Casa Matriz (r0)
-    # Hacia LAN de Sucursal 1 a través del IP WAN de r1
-    r0.cmd('ip route add 10.0.1.0/24 via 192.168.100.1 dev r0-eth0')
-    # Hacia LAN de Sucursal 2 a través del IP WAN de r2
-    r0.cmd('ip route add 10.0.2.0/24 via 192.168.100.9 dev r0-eth1')
+    print("*** Iniciando Red")
+    net.start()
 
-    # Rutas en Router Sucursal 1 (r1)
-    # Ruta por defecto hacia Casa Matriz (r0) para alcanzar otras sucursales o "internet"
-    r1.cmd('ip route add default via 192.168.100.6 dev r1-wan0')
+    print("*** Configuring hosts")
+    for host in hosts:
+        host.cmd('ip route add default via 10.0.%d.1' % (hosts.index(host)+1))
 
-    # Rutas en Router Sucursal 2 (r2)
-    # Ruta por defecto hacia Casa Matriz (r0)
-    r2.cmd('ip route add default via 192.168.100.14 dev r2-wan0')
-
-    # Las rutas por defecto de h1 y h2 ya se configuraron al crear los hosts.
-
-    info('*** ¡Red Lista! Iniciando CLI de Mininet...\n')
+    print("*** Iniciando CLI")
     CLI(net)
 
-    info('*** Deteniendo Red\n')
+    print("*** Deteniendo red")
     net.stop()
 
 if __name__ == '__main__':
     setLogLevel('info')
-    nationalWanNetwork()
+    dynamicNationalWanNetwork()
